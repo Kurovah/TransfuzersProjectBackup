8c096f5c7df4905ded03d0d40bcb46b1ba89454b
Sandstormix <45597290+Darudens@users.noreply.github.com>
2021-08-15 20:52:15 +0100

    Tower Placement

---
+++ Assets/Scenes/Dariusz/Scripts/BuildingManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+using UnityEditor.PackageManager;
+using UnityEngine.Diagnostics;
+
+public class BuildingManager : MonoBehaviour
+{
+    
+    [SerializeField] private Transform towerTest;
+    [SerializeField] private LayerMask layerMask;
+    [SerializeField] private Camera mainCamera;
+
+    private TowerHUD _hud;
+
+    void Awake()
+    {
+        _hud = TowerHUD.Instance;
+    }
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 towerPosition = -Vector3.one;
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                towerPosition = hit.point;
+            }
+            Instantiate(towerTest, towerPosition, Quaternion.identity);
+        }
+    }
+}
---
+++ Assets/Scenes/Dariusz/Scripts/TowerHUD.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TowerHUD : MonoBehaviour
+{
+    [SerializeField] private Button button;
+
+    public static TowerHUD Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null) { Instance = this;  }
+        else { Destroy(gameObject); }
+    }
+
+    public bool GetButtonStatus()
+    {
+        return button.IsActive();
+    }
+}
