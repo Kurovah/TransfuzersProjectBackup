5a1fe8a8e86a5c34725097ba78146fa815309784
Kurovah <45458052+Kurovah@users.noreply.github.com>
2021-09-15 23:16:29 +0100

    small change

---
+++ Assets/Scripts/UI elements/ResearchAreaSlider.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResearchAreaSlider : MonoBehaviour
+{
+    Vector3 initialMousePos, initialObjectPos ,MouseOffset;
+    public RectTransform r;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        initialMousePos = Input.mousePosition;
+        initialObjectPos = r.position;
+        Debug.Log("Cllicked");
+    }
+
+    private void OnMouseDrag()
+    {
+        MouseOffset = Input.mousePosition;
+        r.position = initialObjectPos + (initialMousePos + MouseOffset);
+    }
+}
