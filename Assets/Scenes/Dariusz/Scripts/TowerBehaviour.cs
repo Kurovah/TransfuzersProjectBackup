3285beec623831a41db0e92a439dae0545eb20dc
Kurovah <45458052+Kurovah@users.noreply.github.com>
2021-09-29 23:33:34 +0100

    a

--- Assets/Scenes/Dariusz/Scripts/TowerBehaviour.cs
+++ Assets/Scenes/Dariusz/Scripts/TowerBehaviour.cs
@@ -2,8 +2,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Rendering.HighDefinition;
-using UnityEngine.SocialPlatforms;
 
 public class TowerBehaviour : MonoBehaviour
 {
